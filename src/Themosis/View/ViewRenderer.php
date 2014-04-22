908c1ffc085938a142bf09ca143ceb1de9273db3
Julien Lambé <julien@jlambe.be>
2014-04-22 17:24:48 +0200

    Remove the eval() function.

--- src/Themosis/View/ViewRenderer.php
+++ src/Themosis/View/ViewRenderer.php
@@ -16,7 +16,7 @@ class ViewRenderer
 	private $viewID;
 
 	/**
-	 * View temp path
+	 * Temp view file path
 	*/
 	protected $path;
 
@@ -50,20 +50,24 @@ class ViewRenderer
 		// Extract sent datas
 		extract($this->view->getDatas());
 
-		// Grab the view content
-		$content = $this->load();
+        // Create the temp file and fill it with the view
+        // content.
+        $this->path = $this->setViewFile($this->load());
 
 		// Compile the view
 		try
 		{
-			// Eval view content
-			eval('?>'.$content);
+			// Include the view
+            include($this->path);
 
 		} catch (Exception $e)
 		{
 			ob_get_clean();
 			throw $e;
 		}
+
+        // Remove temporary file reference.
+        unlink($this->path);
 
 		// Return the compiled view and terminate the output buffer
 		return ob_get_clean();
@@ -72,7 +76,7 @@ class ViewRenderer
 	/**
 	 * Load the view content.
 	 *
-     * @TODO Not implemented yet.
+     * @TODO Implement cache
 	 * @return string The view cached content.
 	 */
 	private function load()
@@ -83,5 +87,39 @@ class ViewRenderer
 			return static::$cache[$this->viewID] = $this->view->get();
 		}
 	}
+
+    /**
+     * Create a temporary view file, set its content and returns
+     * the file path.
+     *
+     * @param string $content The temporary file content.
+     * @throws ViewException
+     * @return string|bool The temporary view file path. False if error when creating the temporary file.
+     */
+    private function setViewFile($content)
+    {
+        if(!is_string($content)){
+            throw new ViewException("Invalid view content. Can't create a temporary view file.");
+        }
+
+        // Create the temporary file
+        $tmp = tempnam(sys_get_temp_dir(), $this->view->getViewID());
+
+        // Check if we get a string so it makes sure
+        // the file exists.
+        if(is_string($tmp) && !empty($tmp)){
+
+            // The temporary file is  empty.
+            // Add the view content to the file.
+            $handle = fopen($tmp, 'w');
+            fwrite($handle, $content);
+            fclose($handle);
+
+            // Return the file path
+            return $tmp;
+        }
+
+        return false;
+    }
 
 }
