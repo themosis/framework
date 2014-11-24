9b303775a33f1db176a18cc2f3aea54d6887f744
Julien Lambé <julien@jlambe.be>
2014-11-24 11:49:34 +0100

    Enable core filters.

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -38,7 +38,12 @@ $app->instance('app', $app);
 /*----------------------------------------------------*/
 Themosis\Facades\Facade::clearResolvedInstances();
 
-//Themosis\Facades\Facade::setFacadeApplication($app);
+Themosis\Facades\Facade::setFacadeApplication($app);
+
+/*----------------------------------------------------*/
+// Register Facade Aliases To Full Classes
+/*----------------------------------------------------*/
+$app->registerCoreContainerAliases();
 
 /*----------------------------------------------------*/
 // Set application configurations.
@@ -48,82 +53,82 @@ do_action('themosis_configurations');
 /*----------------------------------------------------*/
 // Register framework view paths.
 /*----------------------------------------------------*/
-//add_filter('themosisViewPaths', function($paths){
-//
-//    $paths[] = themosis_path('sys').'Metabox'.DS.'Views'.DS;
-//    $paths[] = themosis_path('sys').'Page'.DS.'Views'.DS;
-//    $paths[] = themosis_path('sys').'Field'.DS.'Fields'.DS.'Views'.DS;
-//    $paths[] = themosis_path('sys').'Route'.DS.'Views'.DS;
-//
-//    return $paths;
-//
-//});
+add_filter('themosisViewPaths', function($paths){
+
+    $paths[] = themosis_path('sys').'Metabox'.DS.'Views'.DS;
+    $paths[] = themosis_path('sys').'Page'.DS.'Views'.DS;
+    $paths[] = themosis_path('sys').'Field'.DS.'Fields'.DS.'Views'.DS;
+    $paths[] = themosis_path('sys').'Route'.DS.'Views'.DS;
+
+    return $paths;
+
+});
 
 /*----------------------------------------------------*/
 // Register framework asset paths.
 /*----------------------------------------------------*/
-//add_filter('themosisAssetPaths', function($paths){
-//
-//    $coreUrl = themosis_plugin_url().'/src/Themosis/_assets';
-//    $paths[$coreUrl] = themosis_path('sys').'_assets';
-//
-//    return $paths;
-//
-//});
+add_filter('themosisAssetPaths', function($paths){
+
+    $coreUrl = themosis_plugin_url().'/src/Themosis/_assets';
+    $paths[$coreUrl] = themosis_path('sys').'_assets';
+
+    return $paths;
+
+});
 
 
 /*----------------------------------------------------*/
 // Register framework media image size.
 /*----------------------------------------------------*/
-//add_image_size('_themosis_media', 100, 100, true);
-//
-//add_filter('image_size_names_choose', function($sizes){
-//
-//    $sizes['_themosis_media'] = __('Themosis Media Thumbnail', THEMOSIS_FRAMEWORK_TEXTDOMAIN);
-//
-//    return $sizes;
-//});
+add_image_size('_themosis_media', 100, 100, true);
+
+add_filter('image_size_names_choose', function($sizes){
+
+    $sizes['_themosis_media'] = __('Themosis Media Thumbnail', THEMOSIS_FRAMEWORK_TEXTDOMAIN);
+
+    return $sizes;
+});
 
 /*----------------------------------------------------*/
 // Allow developers to add parameters to
 // the admin global JS object.
 /*----------------------------------------------------*/
-//add_action('admin_head', function(){
-//
-//    $datas = apply_filters('themosisAdminGlobalObject', array());
-//
-//    $output = "<script type=\"text/javascript\">\n\r";
-//    $output.= "//<![CDATA[\n\r";
-//    $output.= "var thfmk_themosis = {\n\r";
-//
-//    if (!empty($datas))
-//    {
-//        foreach ($datas as $key => $value)
-//        {
-//            $output.= $key.": ".json_encode($value).",\n\r";
-//        }
-//    }
-//
-//    $output.= "};\n\r";
-//    $output.= "//]]>\n\r";
-//    $output.= "</script>";
-//
-//    // Output the datas.
-//    echo($output);
-//
-//});
+add_action('admin_head', function(){
+
+    $datas = apply_filters('themosisAdminGlobalObject', array());
+
+    $output = "<script type=\"text/javascript\">\n\r";
+    $output.= "//<![CDATA[\n\r";
+    $output.= "var thfmk_themosis = {\n\r";
+
+    if (!empty($datas))
+    {
+        foreach ($datas as $key => $value)
+        {
+            $output.= $key.": ".json_encode($value).",\n\r";
+        }
+    }
+
+    $output.= "};\n\r";
+    $output.= "//]]>\n\r";
+    $output.= "</script>";
+
+    // Output the datas.
+    echo($output);
+
+});
 
 /*----------------------------------------------------*/
 // Register framework core assets URL to
 // admin global object.
 /*----------------------------------------------------*/
-//add_filter('themosisAdminGlobalObject', function($paths){
-//
-//    $paths['_themosisAssets'] = themosis_plugin_url().'/src/Themosis/_assets';
-//
-//    return $paths;
-//
-//});
+add_filter('themosisAdminGlobalObject', function($paths){
+
+    $paths['_themosisAssets'] = themosis_plugin_url().'/src/Themosis/_assets';
+
+    return $paths;
+
+});
 
 /*----------------------------------------------------*/
 // Enqueue frameworks assets.
@@ -137,4 +142,4 @@ do_action('themosis_configurations');
 /*----------------------------------------------------*/
 // Bootstrap application.
 /*----------------------------------------------------*/
-//do_action('themosis_bootstrap');
+do_action('themosis_bootstrap');
