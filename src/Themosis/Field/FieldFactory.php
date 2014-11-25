3805b2e9a6269e55794240a871f26c54d1ce8e42
Julien Lambé <julien@jlambe.be>
2014-11-25 14:00:16 +0100

    Password Field handles custom class property.

--- src/Themosis/Field/FieldFactory.php
+++ src/Themosis/Field/FieldFactory.php
@@ -59,7 +59,7 @@ class FieldFactory {
     {
         $properties = compact('name');
 
-        $properties = array_merge($extras, $properties);
+        $properties = array_merge(array('class' => 'large-text'), $extras, $properties);
 
         return $this->make('Themosis\\Field\\Fields\\PasswordField', $properties);
     }
