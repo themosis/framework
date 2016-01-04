0ec7da56379251ba0aadb21742d878750eedab62
Julien Lambé <julien@jlambe.be>
2016-01-04 23:21:18 +0100

    Add webpack to handle file compilation.

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var webpack = require('webpack'),
+    path = require('path'),
+    branch = require('git-branch'),
+    ExtractTextPlugin = require('extract-text-webpack-plugin'),
+    srcPath = path.join(__dirname, 'src/Themosis/_assets'),
+    autoprefixer = require('autoprefixer'),
+    precss = require('precss');
+
+module.exports = {
+    target: 'web',
+    cache: true,
+    entry: {
+        '_themosis-core': path.join(srcPath, 'stylus/screen.styl')
+    },
+    output: {
+        path: srcPath,
+        publicPath: '',
+        filename: '[name].js'
+    },
+    resolve: {
+      extensions: ['', '.js', '.css', '.styl']
+    },
+    module: {
+      loaders: [
+          // Extract CSS files
+          {
+              test: /\.css$/,
+              loader: ExtractTextPlugin.extract('css', 'css-loader')
+          },
+          // Extract STYLUS files
+          {
+              test: /\.styl$/,
+              loader: ExtractTextPlugin.extract('stylus', 'css-loader?minimize!postcss-loader!stylus-loader')
+          }
+      ]
+    },
+    postcss: function()
+    {
+        return [autoprefixer, precss];
+    },
+    plugins: [
+        new ExtractTextPlugin('./css/[name].css', {allChunks: true}),
+        new webpack.NoErrorsPlugin()
+    ]
+};
