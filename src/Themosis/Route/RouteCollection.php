63f6010624b87e7ecf7d8ed1b3f94ab3fe8bf30a
Dennis Stolmeijer <dstollie@hotmail.com>
2016-04-07 02:50:40 +0200

    - The 404 conditional tag will now also be used when no route can be found. - When checking which route matches the current request, first check the non-wordpress routes. Wordpress sees the non-wordpress routes as the homepage and we can avoid it using this by first matching the non-wordpress routes and after that the wordpress routes

--- src/Themosis/Route/RouteCollection.php
+++ src/Themosis/Route/RouteCollection.php
@@ -3,6 +3,7 @@ namespace Themosis\Route;
 
 use Illuminate\Routing\Matching\SchemeValidator;
 use Illuminate\Routing\RouteCollection as IlluminateRouteCollection;
+use Illuminate\Support\Arr;
 
 class RouteCollection extends IlluminateRouteCollection {
     /**
@@ -24,5 +25,30 @@ class RouteCollection extends IlluminateRouteCollection {
         }
 
         $this->allRoutes[$method.$domainAndUri] = $route;
+    }
+
+    /**
+     * Determine if a route in the array matches the request.
+     *
+     * @param  array  $routes
+     * @param  \Illuminate\http\Request  $request
+     * @param  bool  $includingMethod
+     * @return \Illuminate\Routing\Route|null
+     */
+    protected function check(array $routes, $request, $includingMethod = true)
+    {
+        // Reorganize the order of the routes so that the normal routes will be checked first before the conditional WordPress routes
+        $reorganizedRoutes = Arr::sort($routes, function(Route $route) {
+            return $route->condition() != null;
+        });
+
+        $foundRoute = parent::check($reorganizedRoutes, $request, $includingMethod);
+
+        // If no route found, check if the 404 route is set, if so, return that route as our found route
+        if(!$foundRoute && isset($routes['404'])) {
+            $foundRoute = $routes['404'];
+        }
+
+        return $foundRoute;
     }
 }
