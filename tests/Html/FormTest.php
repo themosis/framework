f006c3616a4fd0e6e37830b2371b8a4f36bb7039
Julien Lambé <julien@jlambe.be>
2016-04-23 09:21:40 +0200

    Fix form default test.

--- tests/Html/FormTest.php
+++ tests/Html/FormTest.php
@@ -5,29 +5,17 @@ use Themosis\Html\HtmlBuilder as Html;
 use Themosis\Session\Session;
 use Themosis\Core\Request;
 
-class FormTest extends PHPUnit_Framework_TestCase
+class FormTest extends WP_UnitTestCase
 {
     /**
-     * Initialize a FormBuilder instance.
-     */
-    public function setUp()
-    {
-        WP_Mock::setUp();
-    }
-
-    public function tearDown()
-    {
-        WP_Mock::tearDown();
-    }
-
-    /**
      * Return a form instance working on a domain without SSL
-     * and no query arguments and sub-uri. (Like a home page)
+     * and no query arguments and sub-uri. (Like a home page).
      */
     protected function getFormNonSSLDomain()
     {
         $request = Request::createFromBase(Request::create('http://somedomain.com'));
         $html = new Html();
+
         return new Form($html, $request);
     }
 
@@ -36,20 +24,10 @@ class FormTest extends PHPUnit_Framework_TestCase
      */
     public function testOpenTagDefaultArgsNonSSL()
     {
-        WP_Mock::wpPassthruFunction('esc_url');
-
-        WP_Mock::wpFunction('is_ssl', [
-            'return'    => false
-        ]);
-
-        WP_Mock::wpFunction('wp_nonce_field', [
-            'args'      => [Session::nonceAction, Session::nonceName, true, false],
-            'return'    => '<input type="hidden" id="_themosisnonce" name="_themosisnonce" value="123abc789d"><input type="hidden" name="_wp_http_referer" value="/">'
-        ]);
-
         // Default, no arguments.
         $form = $this->getFormNonSSLDomain();
         $open = $form->open();
-        $this->assertEquals('<form action="http://somedomain.com/" method="POST" accept-charset="UTF-8"><input type="hidden" id="_themosisnonce" name="_themosisnonce" value="123abc789d"><input type="hidden" name="_wp_http_referer" value="/">', $open);
+        $nonce = wp_create_nonce(Session::nonceAction);
+        $this->assertEquals('<form action="http://somedomain.com/" method="POST" accept-charset="UTF-8"><input type="hidden" id="_themosisnonce" name="_themosisnonce" value="'.$nonce.'" /><input type="hidden" name="_wp_http_referer" value="" />', $open);
     }
 }
