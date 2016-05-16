02b0fd378d104fc5dcef4908c1f2ad213d3f4fad
Julien Lambé <julien@jlambe.be>
2016-05-16 19:03:12 +0200

    Add Filter facade.

---
+++ src/Themosis/Facades/Filter.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Themosis\Facades;
+
+class Filter extends Facade
+{
+    /**
+     * Return the service provider key responsible for the asset class.
+     * The key must be the same as the one used when registering
+     * the service provider.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'filter';
+    }
+}
