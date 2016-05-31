7ec02fa71355c3e3d87e71755988ccc3b5bc077c
Julien Lambé <julien@jlambe.be>
2016-06-01 00:04:59 +0200

    Update BaseController to reflect new API.

--- src/Themosis/Route/BaseController.php
+++ src/Themosis/Route/BaseController.php
@@ -6,13 +6,4 @@ use Illuminate\Routing\Controller;
 
 class BaseController extends Controller
 {
-    /**
-     * Setup the layout used by the controller.
-     */
-    protected function setupLayout()
-    {
-        if (!is_null($this->layout)) {
-            $this->layout = $this->view->make($this->layout);
-        }
-    }
 }
