d588bfde599ca40293ec2c579b4699a16ff9b202
Daniel Gadd <DJGadd@DJGadd.local>
2017-02-07 15:52:41 +0000

    * Helps if you update the Facade...

--- src/Themosis/Facades/Config.php
+++ src/Themosis/Facades/Config.php
@@ -2,7 +2,9 @@
 
 namespace Themosis\Facades;
 
-class Config extends Facade
+use ArrayAccess;
+
+class Config extends Facade implements ArrayAccess
 {
     /**
      * Return the service provider key responsible for the config class.
@@ -14,5 +16,56 @@ class Config extends Facade
     protected static function getFacadeAccessor()
     {
         return 'config.factory';
+    }
+
+    /**
+     * Determines if an offset exists in a config for ArrayAccess
+     *
+     * @param  mixed  $offset
+     *
+     * @return  boolean
+     */
+    public function offsetExists($offset)
+    {
+      return self::offsetExists($offset);
+    }
+
+    /**
+     * Returns an offset from the config for ArrayAccess interface
+     *
+     * @param  mixed  $offset
+     *
+     * @return  mixed
+     */
+    public function offsetGet($offset)
+    {
+        return self::offsetGet($offset);
+    }
+
+    /**
+     * Implementing for ArrayAccess, doesn't do anything because configs are read
+     * only.
+     *
+     * @param  mixed  $offset
+     * @param  mixed  $value
+     *
+     * @return void
+     */
+    public function offsetSet($offset, $value)
+    {
+        self::offsetSet($offset, $value);
+    }
+
+    /**
+     * Implementing for ArrayAccess, doesn't do anything because configs are read
+     * only.
+     *
+     * @param  mixed  $offset
+     *
+     * @return void
+     */
+    public function offsetUnset($offset)
+    {
+        self::offsetSet($offset);
     }
 }
