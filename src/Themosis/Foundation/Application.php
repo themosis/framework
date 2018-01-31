a69e71c53dcf1477df9a1f298f3ff8b897de8dc2
Julien Lambé <julien@themosis.com>
2018-02-01 00:20:31 +0100

    Start laravel implementation of service providers.

--- src/Themosis/Foundation/Application.php
+++ src/Themosis/Foundation/Application.php
@@ -4,6 +4,9 @@ namespace Themosis\Foundation;
 
 use Illuminate\Container\Container;
 use Illuminate\Contracts\Foundation\Application as ApplicationContract;
+use Illuminate\Support\Arr;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
@@ -11,33 +14,93 @@ use Symfony\Component\HttpKernel\HttpKernelInterface;
 class Application extends Container implements ApplicationContract, HttpKernelInterface
 {
     /**
+     * Application version.
+     *
+     * @var string
+     */
+    const VERSION = '1.4.0';
+
+    /**
+     * Base path of the framework.
+     *
+     * @var string
+     */
+    protected $basePath;
+
+    /**
+     * The deferred services and their providers.
+     *
+     * @var array
+     */
+    protected $deferredServices = [];
+
+    /**
+     * All of the registered service providers.
+     *
+     * @var array
+     */
+    protected $serviceProviders = [];
+
+    /**
+     * The names of the loaded service providers.
+     *
+     * @var array
+     */
+    protected $loadedProviders = [];
+
+    /**
+     * Indicates if the application has booted.
+     *
+     * @var bool
+     */
+    protected $booted = false;
+
+    public function __construct()
+    {
+    }
+
+    /**
      * Get the version number of the application.
      *
      * @return string
      */
     public function version()
     {
-        // TODO: Implement version() method.
+        return static::VERSION;
     }
 
     /**
      * Get the base path of the Laravel installation.
      *
+     * @param string $path Optional path to append to the base path.
+     *
      * @return string
      */
-    public function basePath()
+    public function basePath($path = '')
     {
-        // TODO: Implement basePath() method.
+        return $this->basePath.($path ? DIRECTORY_SEPARATOR.$path : $path);
     }
 
     /**
      * Get or check the current application environment.
      *
-     * @return string
+     * @return string|bool
      */
     public function environment()
     {
-        // TODO: Implement environment() method.
+        if (func_num_args() > 0) {
+            $patterns = is_array(func_get_arg(0)) ? func_get_arg(0) : func_get_args();
+
+            foreach ($patterns as $pattern) {
+                if (Str::is($pattern, $this['env'])) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        return $this['env'];
     }
 
     /**
@@ -47,7 +110,7 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
      */
     public function runningInConsole()
     {
-        // TODO: Implement runningInConsole() method.
+        return php_sapi_name() == 'cli' || php_sapi_name() == 'phpdbg';
     }
 
     /**
@@ -57,7 +120,7 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
      */
     public function isDownForMaintenance()
     {
-        // TODO: Implement isDownForMaintenance() method.
+        return file_exists(ABSPATH.'.maintenance') || wp_installing();
     }
 
     /**
@@ -79,7 +142,17 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
      */
     public function registerDeferredProvider($provider, $service = null)
     {
-        // TODO: Implement registerDeferredProvider() method.
+        if ($service) {
+            unset($this->deferredServices[$service]);
+        }
+
+        $this->register($instance = new $provider($this));
+
+        if (!$this->booted) {
+            $this->booting(function () use ($instance) {
+                $this->bootProvider($instance);
+            });
+        }
     }
 
     /**
@@ -90,6 +163,20 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
     public function boot()
     {
         // TODO: Implement boot() method.
+    }
+
+    /**
+     * Boot the given service provider.
+     *
+     * @param ServiceProvider $provider
+     *
+     * @return mixed
+     */
+    protected function bootProvider(ServiceProvider $provider)
+    {
+        if (method_exists($provider, 'boot')) {
+            return $this->call([$provider, 'boot']);
+        }
     }
 
     /**
@@ -160,10 +247,86 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
      * @param  \Illuminate\Support\ServiceProvider|string $provider
      * @param  array $options
      * @param  bool $force
+     *
      * @return \Illuminate\Support\ServiceProvider
      */
     public function register($provider, $options = [], $force = false)
     {
-        // TODO: Implement register() method.
+        if (($registered = $this->getProvider($provider)) && !$force) {
+            return $registered;
+        }
+
+        // If the given "provider" is a string, we will resolve it, passing in the
+        // application instance automatically for the developer. This is simply
+        // a more convenient way of specifying your service provider classes.
+        if (is_string($provider)) {
+            $provider = $this->resolveProvider($provider);
+        }
+
+        if (method_exists($provider, 'register')) {
+            $provider->register();
+        }
+
+        $this->markAsRegistered($provider);
+
+        // If the application has already booted, we will call this boot method on
+        // the provider class so it has an opportunity to do its boot logic and
+        // will be ready for any usage by this developer's application logic.
+        if ($this->booted) {
+            $this->bootProvider($provider);
+        }
+
+        return $provider;
+    }
+
+    /**
+     * Get the registered service provider instance if it exists.
+     *
+     * @param ServiceProvider|string $provider
+     *
+     * @return ServiceProvider|null
+     */
+    public function getProvider($provider)
+    {
+        return array_values($this->getProviders($provider))[0] ?? null;
+    }
+
+    /**
+     * Get the registered service provider instances if any exist.
+     *
+     * @param ServiceProvider|string $provider
+     *
+     * @return array
+     */
+    public function getProviders($provider)
+    {
+        $name = is_string($provider) ? $provider : get_class($provider);
+
+        return Arr::where($this->serviceProviders, function ($value) use ($name) {
+            return $value instanceof $name;
+        });
+    }
+
+    /**
+     * Resolve a service provider instance from the class name.
+     *
+     * @param string $provider
+     *
+     * @return ServiceProvider
+     */
+    public function resolveProvider($provider)
+    {
+        return new $provider($this);
+    }
+
+    /**
+     * Mark the given provider as registered.
+     *
+     * @param ServiceProvider $provider
+     */
+    protected function markAsRegistered($provider)
+    {
+        $this->serviceProviders[] = $provider;
+        $this->loadedProviders[get_class($provider)] = true;
     }
 }
