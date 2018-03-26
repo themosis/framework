3543eb1a3cca6f0d08426247ac4b3333cb610855
Julien Lambé <julien@themosis.com>
2018-03-26 17:57:14 +0200

    Add alias loader tests.

---
+++ tests/Core/AliasLoaderTest.php
@@ -0,0 +1,37 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Themosis\Core\AliasLoader;
+
+class AliasLoaderTest extends TestCase
+{
+    public function testLoaderCanBeCreatedAndRegisteredOnce()
+    {
+        $loader = AliasLoader::getInstance(['foo' => 'bar']);
+
+        $this->assertEquals(['foo' => 'bar'], $loader->getAliases());
+        $this->assertFalse($loader->isRegistered());
+
+        $loader->register();
+        $this->assertTrue($loader->isRegistered());
+    }
+
+    public function testGetInstanceCreateOneInstance()
+    {
+        $loader = AliasLoader::getInstance(['some' => 'thing']);
+        $this->assertSame($loader, AliasLoader::getInstance());
+    }
+
+    public function testAliasAreRegisteredAfterInstantiation()
+    {
+        $loader = AliasLoader::getInstance(['bar' => 'baz']);
+        $loader->alias('foo', 'doe');
+
+        // Still same instance on all tests.
+        $this->assertEquals([
+            'bar' => 'baz',
+            'foo' => 'doe',
+            'some' => 'thing'
+        ], $loader->getAliases());
+    }
+}
