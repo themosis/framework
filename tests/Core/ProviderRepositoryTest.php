d1dd918c403b6b054abb785bd2e46d34d763e82e
Julien Lambé <julien@themosis.com>
2018-04-05 11:38:34 +0200

    Complete ProviderRepository tests.

--- tests/Core/ProviderRepositoryTest.php
+++ tests/Core/ProviderRepositoryTest.php
@@ -3,6 +3,8 @@
 namespace Themosis\Tests\Core;
 
 use PHPUnit\Framework\TestCase;
+use Themosis\Core\Application;
+use Themosis\Core\ProviderRepository;
 
 class ProviderRepositoryTest extends TestCase
 {
@@ -97,5 +99,57 @@ class ProviderRepositoryTest extends TestCase
         ]);
 
         $repository->load(['foo']);
+    }
+
+    public function testShouldRecompileReturnsCorrectValue()
+    {
+        $repo = new ProviderRepository(
+            new Application(),
+            $this->getMockBuilder('Illuminate\Filesystem\Filesystem')->getMock(),
+            __DIR__.'/services.php'
+        );
+
+        $this->assertTrue($repo->shouldRecompile(null, []));
+        $this->assertTrue($repo->shouldRecompile(['providers' => ['foo']], ['foo', 'bar']));
+        $this->assertFalse($repo->shouldRecompile(['providers' => ['foo']], ['foo']));
+    }
+
+    public function testLoadManifestReturnsParsedJSON()
+    {
+        $repo = new ProviderRepository(
+            new Application(),
+            $files = $this->getMockBuilder('Illuminate\Filesystem\Filesystem')
+                ->setMethods(['exists', 'getRequire'])
+                ->getMock(),
+            __DIR__.'/services.php'
+        );
+
+        $files->expects($this->once())
+            ->method('exists')
+            ->with(__DIR__.'/services.php')
+            ->willReturn(true);
+        $files->expects($this->once())
+            ->method('getRequire')
+            ->with(__DIR__.'/services.php')
+            ->willReturn($array = ['users' => ['joe' => true], 'when' => []]);
+
+        $this->assertEquals($array, $repo->loadManifest());
+    }
+
+    public function testWriteManifestStoresToProperLocation()
+    {
+        $repo = new ProviderRepository(
+            new Application(),
+            $files = $this->getMockBuilder('Illuminate\Filesystem\Filesystem')
+                ->setMethods(['put'])
+                ->getMock(),
+            __DIR__.'/services.php'
+        );
+
+        $files->expects($this->once())
+            ->method('put')
+            ->with(__DIR__.'/services.php', '<?php return '.var_export(['foo'], true).';');
+        $result = $repo->writeManifest(['foo']);
+        $this->assertEquals(['foo', 'when' => []], $result);
     }
 }
