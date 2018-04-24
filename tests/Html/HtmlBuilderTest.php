0995e5d5a0f9b132a4f8e6d73429e180795908bd
Julien Lambé <julien@themosis.com>
2018-04-24 17:11:54 +0200

    Add HtmlBuilder test for entities and special methods.

--- tests/Html/HtmlBuilderTest.php
+++ tests/Html/HtmlBuilderTest.php
@@ -18,5 +18,38 @@ class HtmlBuilderTest extends TestCase
                 'method' => 'post'
             ])
         );
+
+        $this->assertEquals(
+            'class="some-class" required checked min="0" max="255"',
+            $html->attributes([
+                'class' => 'some-class',
+                'required',
+                'checked',
+                'min' => 0,
+                'max' => 255
+            ])
+        );
+    }
+
+    public function testHtmlBuilderCanConvertCharactersToEntities()
+    {
+        $html = new HtmlBuilder();
+
+        $this->assertEquals(
+            "Un &#039;apostrophe&#039; en &lt;strong&gt;gras&lt;/strong&gt;",
+            $html->entities('Un \'apostrophe\' en <strong>gras</strong>')
+        );
+
+        $this->assertEmpty($html->entities("\x8F!!!"));
+    }
+
+    public function testHtmlBuilderCanConvertSpecialCharacetersToHtmlEntities()
+    {
+        $html = new HtmlBuilder();
+
+        $this->assertEquals(
+            '&lt;a href=&#039;test&#039;&gt;Test&lt;/a&gt;',
+            $html->special("<a href='test'>Test</a>")
+        );
     }
 }
