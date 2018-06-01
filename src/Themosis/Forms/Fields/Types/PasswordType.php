3fd51acd10ff98a60268a39b247e49fd3f991e10
Julien Lambé <julien@themosis.com>
2018-06-01 17:33:33 +0200

    Add password type field.

---
+++ src/Themosis/Forms/Fields/Types/PasswordType.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Themosis\Forms\Fields\Types;
+
+class PasswordType extends BaseType
+{
+    /**
+     * PasswordType field view.
+     *
+     * @var string
+     */
+    protected $view = 'types.password';
+}
