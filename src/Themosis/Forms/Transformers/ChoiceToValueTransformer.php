c8a919e1555e51d9e9732c06daa4278ea070022f
Julien Lambé <julien@themosis.com>
2018-06-25 16:49:29 +0200

    Add phpdoc.

--- src/Themosis/Forms/Transformers/ChoiceToValueTransformer.php
+++ src/Themosis/Forms/Transformers/ChoiceToValueTransformer.php
@@ -6,11 +6,25 @@ use Themosis\Forms\Contracts\DataTransformerInterface;
 
 class ChoiceToValueTransformer implements DataTransformerInterface
 {
+    /**
+     * @inheritdoc
+     *
+     * @param string|array $data
+     *
+     * @return string|array
+     */
     public function transform($data)
     {
         return $data;
     }
 
+    /**
+     * @inheritdoc
+     *
+     * @param string|array $data
+     *
+     * @return string|array
+     */
     public function reverseTransform($data)
     {
         return $data;
