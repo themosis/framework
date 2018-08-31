f220cb83ddeb4c07a966e901e0285bb919c82191
Julien Lambé <julien@themosis.com>
2018-08-31 15:30:49 +0200

    Add Blade @template directive. Similar to get_template_part().

--- src/Themosis/View/BladeServiceProvider.php
+++ src/Themosis/View/BladeServiceProvider.php
@@ -46,6 +46,48 @@ class BladeServiceProvider extends ServiceProvider
             return '<?php $_query = (is_array('.$expression.')) ? new \WP_Query('.$expression.') : '.$expression.'; if ($_query->have_posts()) { while ($_query->have_posts()) { $_query->the_post(); ?>';
         });
 
+        /**
+         * Simulate a WordPress get_template_part() behavior using custom views.
+         *
+         * Examples:
+         * "@template('parts.content', get_post_type())"
+         * "@template('parts.content', 'page')"
+         *
+         * In the first example, the view factory will try to include a "dynamic"
+         * view with the following path "parts.content-post" or "parts.content-attachment".
+         *
+         * In the second example, the view factory tries to include the
+         * "parts.content-page" view.
+         *
+         * We test if the dynamic view exists before trying to render it. If none is found,
+         * we render the view defined by the first argument. In the 2 examples, the view
+         * "parts.content" is rendered and should therefor exists.
+         *
+         * As a third argument, you can pass custom data array to the included view.
+         */
+        Blade::directive('template', function ($expression) {
+            // Get a list of passed arguments.
+            $args = array_map(function ($arg) {
+                return trim($arg, '\/\'\" ()');
+            }, explode(',', $expression));
+
+            // Set the view path.
+            if (isset($args[1])) {
+                if (is_callable($args[1])) {
+                    $args[1] = call_user_func($args[1]);
+                }
+
+                $path = $args[0].'-'.$args[1];
+            } else {
+                $path = $args[0];
+            }
+
+            // Set the view data if defined.
+            $data = 3 === count($args) ? array_pop($args) : '[]';
+
+            return "<?php if (\$__env->exists('{$path}')) { echo \$__env->make('{$path}', {$data}, array_except(get_defined_vars(), array('__data', '__path')))->render(); } else { echo \$__env->make('{$args[0]}', {$data}, array_except(get_defined_vars(), array('__data', '__path')))->render(); } ?>";
+        });
+
         Blade::directive('wp_footer', function () {
             return '<?php wp_footer(); ?>';
         });
