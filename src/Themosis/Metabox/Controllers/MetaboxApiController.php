519ea27d5fe4cb504487ee13bd30fb79e581383c
Julien Lambé <julien@themosis.com>
2018-09-06 11:46:53 +0200

    Setup default metabox rest api for single metabox.

--- src/Themosis/Metabox/Controllers/MetaboxApiController.php
+++ src/Themosis/Metabox/Controllers/MetaboxApiController.php
@@ -3,16 +3,27 @@
 namespace Themosis\Metabox\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Request;
 
 class MetaboxApiController extends Controller
 {
     /**
-     * Handle /metabox/{id} API route.
+     * GET /metabox/{id} API route
      */
-    public function show($id)
+    public function show(Request $request, $id)
     {
-        return response()->json([
-            'id' => $id
-        ]);
+        $abstract = sprintf('themosis.metabox.%s', $id);
+
+        if (app()->bound($abstract)) {
+            $metabox = app('themosis.metabox.'.$id);
+        } else {
+            throw new HttpResponseException(response()->json([
+                'message' => 'The metabox with ID ['.$id.'] does not exist.',
+                'errors' => true
+            ], 404));
+        }
+
+        return response()->json($metabox->toArray());
     }
 }
