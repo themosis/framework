7f3b76fa983a03b7094f3396228e40a08285b39e
Julien Lambé <julien@themosis.com>
2018-10-30 13:28:38 +0100

    Set default taxonomy labels.

--- src/Taxonomy/Factory.php
+++ src/Taxonomy/Factory.php
@@ -22,6 +22,8 @@ class Factory
      *
      * @param string       $slug
      * @param string|array $objects
+     * @param string       $plural
+     * @param string       $singular
      *
      * @throws TaxonomyException
      *
@@ -29,7 +31,30 @@ class Factory
      */
     public function make(string $slug, $objects, string $plural, string $singular): TaxonomyInterface
     {
-        $taxonomy = (new Taxonomy());
+        $taxonomy = (new Taxonomy($slug, (array) $objects))
+            ->setLabels([
+                'name' => $plural,
+                'singular_name' => $singular,
+                'search_items' => sprintf('Search %s', $plural),
+                'popular_items' => sprintf('Popular %s', $plural),
+                'all_items' => sprintf('All %s', $plural),
+                'parent_item' => sprintf('Parent %s', $singular),
+                'parent_item_colon' => sprintf('Parent %s:', $singular),
+                'edit_item' => sprintf('Edit %s', $singular),
+                'view_item' => sprintf('View %s', $singular),
+                'update_item' => sprintf('Update %s', $singular),
+                'add_new_item' => sprintf('Add New %s', $singular),
+                'new_item_name' => sprintf('New %s Name', $singular),
+                'separate_items_with_commas' => sprintf('Separate %s with commas', strtolower($plural)),
+                'add_or_remove_items' => sprintf('Add or remove %s', strtolower($plural)),
+                'choose_from_most_used' => sprintf('Choose from the most used %s', strtolower($plural)),
+                'not_found' => sprintf('No %s found', strtolower($plural)),
+                'no_terms' => sprintf('No %s', strtolower($plural)),
+                'items_list_navigation' => sprintf('%s list navigation', $plural),
+                'items_list' => sprintf('%s list', $plural),
+                'most_used' => 'Most Used',
+                'back_to_items' => sprintf('Back to %s', $plural)
+            ]);
 
         $abstract = sprintf('themosis.taxonomy.%s', $slug);
 
