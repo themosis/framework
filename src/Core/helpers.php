c5d61ddf6e67d56ff1f177bba154cee91a795c41
Julien Lambé <julien@themosis.com>
2018-12-05 13:48:22 +0100

    Fix #546. Complete session support.

--- src/Core/helpers.php
+++ src/Core/helpers.php
@@ -315,6 +315,33 @@ if (! function_exists('route')) {
     }
 }
 
+if (! function_exists('session')) {
+    /**
+     * Get / set the specified session value.
+     *
+     * If an array is passed as the key, we will assume you want to set an array of values.
+     *
+     * @param array|string $key
+     * @param mixed        $default
+     *
+     * @throws \Illuminate\Container\EntryNotFoundException
+     *
+     * @return mixed|\Illuminate\Session\Store|\Illuminate\Session\SessionManager
+     */
+    function session($key = null, $default = null)
+    {
+        if (is_null($key)) {
+            return app('session');
+        }
+
+        if (is_array($key)) {
+            return app('session')->put($key);
+        }
+
+        return app('session')->get($key, $default);
+    }
+}
+
 if (! function_exists('storage_path')) {
     /**
      * Get the path to the storage folder.
