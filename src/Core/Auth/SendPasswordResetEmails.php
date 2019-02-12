c5fa16c7157a1bb8c36ee3e1082429961a527498
Julien Lambé <julien@themosis.com>
2019-02-12 15:00:20 +0100

    Start password reset workflow.

--- src/Core/Auth/SendPasswordResetEmails.php
+++ src/Core/Auth/SendPasswordResetEmails.php
@@ -2,6 +2,10 @@
 
 namespace Themosis\Core\Auth;
 
+use App\Forms\Auth\Password\EmailResetForm;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+
 trait SendPasswordResetEmails
 {
     /**
@@ -11,6 +15,44 @@ trait SendPasswordResetEmails
      */
     public function showLinkRequestForm()
     {
-        return view('auth.passwords.email');
+        $form = $this->form(new EmailResetForm());
+
+        return view('auth.passwords.email', [
+            'form' => $form
+        ]);
+    }
+
+    /**
+     * Send a reset link to the given user.
+     *
+     * @param Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function sendResetLinkEmail(Request $request)
+    {
+        $form = $this->form(new EmailResetForm());
+
+        $form->handleRequest($request);
+
+        if ($form->isNotValid()) {
+            return redirect('password.request')->with([
+                'form' => $form
+            ]);
+        }
+
+        $response = $this->broker()->sendResetLink([
+            'email' => $form->repository()->getFieldByName('email')->getValue()
+        ]);
+    }
+
+    /**
+     * Return the broker to use during password reset.
+     *
+     * @return \Illuminate\Contracts\Auth\PasswordBroker
+     */
+    public function broker()
+    {
+        return Password::broker();
     }
 }
