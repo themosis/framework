2b23a7ad04371c408060d265850449b9a6448eb0
Julien Lambé <julien@themosis.com>
2019-02-12 21:37:00 +0100

    Start reset password.

---
+++ src/Core/Auth/ResetPasswords.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Themosis\Core\Auth;
+
+use Illuminate\Http\Request;
+
+trait ResetPasswords
+{
+    /**
+     * Display the password reset view for the given token.
+     *
+     * If no token is present, display the link request form.
+     *
+     * @param Request $request
+     * @param string|null $token
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function showResetForm(Request $request, $token = null)
+    {
+        return view('auth.passwords.reset')->with([
+            'token' => $token,
+            'email' => ''
+        ]);
+    }
+}
