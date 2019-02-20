5e876a03ff8ee4460657de1986d5c2ad4f7461ea
Julien Lambé <julien@themosis.com>
2019-02-20 21:58:08 +0100

    Add email verification to customer model.

--- src/Core/Auth/User.php
+++ src/Core/Auth/User.php
@@ -8,11 +8,16 @@ use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableInterface;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableInterface;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordInterface;
+use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailInterface;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 use Themosis\Core\Auth\Access\Authorizable;
 
-class User extends Model implements AuthenticatableInterface, AuthorizableInterface, CanResetPasswordInterface
+class User extends Model implements
+    AuthenticatableInterface,
+    AuthorizableInterface,
+    CanResetPasswordInterface,
+    MustVerifyEmailInterface
 {
     use Authenticatable, Authorizable, CanResetPassword, MustVerifyEmail, Notifiable;
 }
