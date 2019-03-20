71a5ac36d6ec8f6fb55a30f545ce059f51c938b8
JasperV <jasper@vanveenhuizen.nl>
2019-03-20 09:36:08 +0100

    add `console serve` command
    requires a server.php in your project root.
    see themosis/themosis for this file.

---
+++ src/Core/Console/ServeCommand.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Themosis\Core\Console;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\ProcessUtils;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Process\PhpExecutableFinder;
+
+class ServeCommand extends Command
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'serve';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Serve the application on the PHP development server';
+
+    /**
+     * The current port offset.
+     *
+     * @var int
+     */
+    protected $portOffset = 0;
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     *
+     * @throws \Exception
+     */
+    public function handle()
+    {
+        chdir(web_path());
+        $this->line("<info>Themosis development server started:</info> <http://{$this->host()}:{$this->port()}>");
+
+        passthru($this->serverCommand(), $status);
+        if ($status && $this->canTryAnotherPort()) {
+            $this->portOffset += 1;
+
+            return $this->handle();
+        }
+
+        return $status;
+    }
+
+    /**
+     * Get the full server command.
+     *
+     * @return string
+     */
+    protected function serverCommand()
+    {
+        return sprintf('%s -S %s:%s %s',
+            ProcessUtils::escapeArgument((new PhpExecutableFinder)->find(false)),
+            $this->host(),
+            $this->port(),
+            ProcessUtils::escapeArgument(base_path('server.php'))
+        );
+    }
+
+    /**
+     * Get the host for the command.
+     *
+     * @return string
+     */
+    protected function host()
+    {
+        return $this->input->getOption('host');
+    }
+
+    /**
+     * Get the port for the command.
+     *
+     * @return string
+     */
+    protected function port()
+    {
+        return $this->input->getOption('port') + $this->portOffset;
+    }
+
+    /**
+     * Check if command has reached its max amount of port tries.
+     *
+     * @return bool
+     */
+    protected function canTryAnotherPort()
+    {
+        return $this->input->getOption('tries') > $this->portOffset;
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [
+            ['host', null, InputOption::VALUE_OPTIONAL, 'The host address to serve the application on', '127.0.0.1'],
+            ['port', null, InputOption::VALUE_OPTIONAL, 'The port to serve the application on', 8000],
+            ['tries', null, InputOption::VALUE_OPTIONAL, 'The max number of ports to attempt to serve from', 10],
+        ];
+    }
+}
