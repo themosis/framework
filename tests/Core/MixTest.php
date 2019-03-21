0a50710af591ba00b1fb8506b9cd75d1244a579c
Kevin Batdorf <kbat82@gmail.com>
2019-03-22 06:01:35 +0700

    refactor mix class

--- tests/Core/MixTest.php
+++ tests/Core/MixTest.php
@@ -2,13 +2,18 @@
 
 namespace Themosis\Tests\Core;
 
-use PHPUnit\Framework\TestCase;
-use Themosis\Core\Application;
 use Themosis\Core\Mix;
+use Themosis\Core\Application;
+use PHPUnit\Framework\TestCase;
 
 class MixTest extends TestCase
 {
-    protected $app;
+	protected $app;
+
+	public function setUp()
+    {
+		$this->getApplication();
+	}
 
     protected function getApplication()
     {
@@ -17,49 +22,41 @@ class MixTest extends TestCase
         }
 
         return $this->app = new Application(dirname(__DIR__));
-    }
+	}
 
     public function testDefaultMixUsage()
     {
-        $this->getApplication();
-
         $mix = app(Mix::class);
         $this->assertEquals(
-            $mix('dist/css/theme.css'),
-            get_home_url(null, 'content/themes/underscore/dist/css/theme.css?id=ba9aead02aea5cc7befb')
+            $mix('css/theme.css'),
+            '/css/theme.css?id=ba9aead02aea5cc7befb'
         );
     }
 
     public function testSpecifyingDirectory()
     {
-        $this->getApplication();
-
         $mix = app(Mix::class);
         $this->assertEquals(
-            $mix('dist/css/theme.css', 'content/themes/underscore'),
-            get_home_url(null, 'content/themes/underscore/dist/css/theme.css?id=ba9aead02aea5cc7befb')
+            $mix('css/theme.css', 'content/themes/underscore/dist'),
+            '/css/theme.css?id=ba9aead02aea5cc7befb'
         );
     }
 
     public function testCallingFromPlugin()
     {
-        $this->getApplication();
-
         $mix = app(Mix::class);
         $this->assertEquals(
-            $mix('dist/css/theme.css', 'content/plugins/timeline'),
-            get_home_url(null, 'content/plugins/timeline/dist/css/theme.css?id=ba9aead02aea5cc7befb')
+            $mix('css/theme.css', 'content/plugins/timeline/dist'),
+            '/css/theme.css?id=ba9aead02aea5cc7befb'
         );
     }
 
     public function testCallingFromWebRoot()
     {
-        $this->getApplication();
-
-        $mix = app(Mix::class);
+		$mix = app(Mix::class);
         $this->assertEquals(
-            $mix('dist/css/theme.css', '/'),
-            get_home_url(null, '/dist/css/theme.css?id=ba9aead02aea5cc7befb')
+            $mix('css/theme.css', '/'),
+            '/css/theme.css?id=ba9aead02aea5cc7befb'
         );
     }
 }
