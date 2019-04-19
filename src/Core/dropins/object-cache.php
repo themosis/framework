ec98843450fc8bd75b4f9f3826e7293da27d343e
Julien Lambé <julien@themosis.com>
2019-04-19 11:58:29 +0200

    Start WordPress cache wrapper.

--- src/Core/dropins/object-cache.php
+++ src/Core/dropins/object-cache.php
@@ -4,11 +4,162 @@
  * Themosis Framework
  * WordPress Object Cache Drop-In
  */
-
 use Illuminate\Support\Facades\Cache;
 
 if (! defined('ABSPATH')) {
     die();
 }
 
-Cache::put('wp', 'test', 3600);
+/**
+ * Sets up global object cache instance.
+ * The used instance is the one defined as the default
+ * cache driver from the application cache configuration.s
+ *
+ * @global \Illuminate\Cache\Repository $wp_object_cache
+ */
+function wp_cache_init()
+{
+    $GLOBALS['wp_object_cache'] = app('cache.store');
+}
+
+/**
+ * Adds data to the cache if the cache key doesn't already exist.
+ *
+ * @param string|int $key
+ * @param mixed      $data
+ * @param string     $group
+ * @param int        $expire
+ *
+ * @return bool True on success. False if cache value is already in the store.
+ */
+function wp_cache_add($key, $data, $group = '', $expire = 0)
+{
+    global $wp_object_cache;
+
+    return $wp_object_cache->add($key, $data, $expire);
+}
+
+/**
+ * Closes the cache.
+ *
+ * @return bool
+ */
+function wp_cache_close()
+{
+    return true;
+}
+
+/**
+ * Decrement numeric cache item's value.
+ *
+ * @param string|int $key
+ * @param int        $offset
+ * @param string     $group
+ *
+ * @return bool|int False on failure. The item's new value on success.
+ */
+function wp_cache_decr($key, $offset = 1, $group = '')
+{
+    global $wp_object_cache;
+
+    return $wp_object_cache->decrement($key, $offset);
+}
+
+/**
+ * Increment numeric cache item's value.
+ *
+ * @param string|int $key
+ * @param int        $offset
+ * @param string     $group
+ *
+ * @return bool|int False on failure. The item's new value on success.
+ */
+function wp_cache_incr($key, $offset = 1, $group = '')
+{
+    global $wp_object_cache;
+
+    return $wp_object_cache->increment($key, $offset);
+}
+
+/**
+ * Removes the cache contents matching key.
+ *
+ * @param string|int $key
+ * @param string     $group
+ *
+ * @return bool True on success. False on failure.
+ */
+function wp_cache_delete($key, $group = '')
+{
+    global $wp_object_cache;
+
+    return $wp_object_cache->delete($key);
+}
+
+/**
+ * Removes all cache items.
+ *
+ * @return bool True on success. False on failure.
+ */
+function wp_cache_flush()
+{
+    global $wp_object_cache;
+
+    return $wp_object_cache->clear();
+}
+
+/**
+ * Retrieve the cache content from the cache by key.
+ *
+ * @param string|int $key
+ * @param string     $group
+ * @param bool       $force
+ * @param null       $found
+ *
+ * @return mixed False on failure. Cache content on success.
+ */
+function wp_cache_get($key, $group = '', $force = false, &$found = null)
+{
+    global $wp_object_cache;
+
+    return $wp_object_cache->get($key);
+}
+
+/**
+ * Store an item in the cache.
+ *
+ * @param string|int $key
+ * @param mixed      $data
+ * @param string     $group
+ * @param int        $expires
+ *
+ * @return bool False on failure. True on success.
+ */
+function wp_cache_set($key, $data, $group = '', $expires = 0)
+{
+    global $wp_object_cache;
+
+    return $wp_object_cache->set($key, $data, $expires);
+}
+
+function wp_cache_replace($key, $data, $group = '', $expire = 0)
+{
+}
+
+function wp_cache_switch_to_blog($blog_id)
+{
+}
+
+function wp_cache_add_global_groups($groups)
+{
+}
+
+/**
+ * Reset internal cache keys and structures.
+ *
+ * @return bool
+ */
+function wp_cache_reset()
+{
+    return true;
+}
