841396fa35636d207c7267336d597e6b89cd45d3
Julien Lambé <julien@themosis.com>
2019-04-19 18:01:26 +0200

    Change dropins tag names for vendor publishing.

--- src/Core/Providers/CoreServiceProvider.php
+++ src/Core/Providers/CoreServiceProvider.php
@@ -60,11 +60,15 @@ class CoreServiceProvider extends AggregateServiceProvider
 
             $this->publishes([
                 __DIR__.'/../Exceptions/views' => resource_path('views/errors/')
-            ], 'themosis-errors');
+            ], 'themosis.errors');
 
             $this->publishes([
                 __DIR__.'/../dropins' => content_path()
-            ], 'themosis-dropins');
+            ], 'themosis.dropins');
+
+            $this->publishes([
+                __DIR__.'/../dropins/object-cache.php' => content_path('object-cache.php')
+            ], 'themosis.dropins.objectcache');
         }
     }
 }
