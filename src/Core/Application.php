6201ef822f2bd0611d17183a899d8ddf952b428c
Julien Lambé <julien@themosis.com>
2019-05-22 15:38:20 +0200

    Update framework version to 2.0.2

--- src/Core/Application.php
+++ src/Core/Application.php
@@ -34,7 +34,7 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
      *
      * @var string
      */
-    const VERSION = '2.0.1';
+    const VERSION = '2.0.2';
 
     /**
      * Application textdomain.
