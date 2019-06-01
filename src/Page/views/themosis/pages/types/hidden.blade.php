40e4108b1cc3f34321796c20df1310544e30d2c8
Julien Lambé <julien@themosis.com>
2019-06-01 19:58:06 +0200

    Fix #671. Correctly use the field value.

--- src/Page/views/themosis/pages/types/hidden.blade.php
+++ src/Page/views/themosis/pages/types/hidden.blade.php
@@ -1,3 +1,3 @@
-<input type="hidden" name="{{ $__field->getName() }}" {!! $__field->attributes($__field->getAttributes()) !!} value="{{ $__field->getRawValue() }}">
-<input type="text" disabled="disabled" name="{{ $__field->getName() }}" class="all-options disabled" value="{{ $__field->getRawValue() }}">
+<input type="hidden" name="{{ $__field->getName() }}" {!! $__field->attributes($__field->getAttributes()) !!} value="{{ $__field->getValue() }}">
+<input type="text" disabled="disabled" name="{{ $__field->getName() }}" class="all-options disabled" value="{{ $__field->getValue() }}">
 @include($__field->getTheme().'.types.includes.info', ['field' => $__field])
--- src/Taxonomy/views/themosis/taxonomy/types/number.blade.php
+++ src/Taxonomy/views/themosis/taxonomy/types/number.blade.php
@@ -1,2 +1,2 @@
-<input type="number" name="{{ $__field->getName() }}" {!! $__field->attributes($__field->getAttributes()) !!} value="{{ $__field->getRawValue() }}">
+<input type="number" name="{{ $__field->getName() }}" {!! $__field->attributes($__field->getAttributes()) !!} value="{{ $__field->getValue() }}">
 @include($__field->getTheme().'.types.includes.info', ['field' => $__field])
