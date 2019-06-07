ee0054f5224a7dfb02520a32fba6aed8ee16db94
Julien Lambé <julien@themosis.com>
2019-06-07 11:08:37 +0200

    Fix #678. Implement form blade directives.

--- src/Forms/FormServiceProvider.php
+++ src/Forms/FormServiceProvider.php
@@ -45,16 +45,35 @@ class FormServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // Render a form.
+        // The directive needs the form instance as a
+        // first parameter.
         Blade::directive('form', function ($expression) {
             return "<?php echo ($expression)->render(); ?>";
         });
 
+        // Render a form open tag.
+        // The directive needs the form instance as a
+        // first parameter.
         Blade::directive('formOpen', function ($expression) {
             return "<?php echo ($expression)->open(); ?>";
         });
 
+        // Render a form close tag.
+        // The directive needs the form instance as a
+        // first parameter.
         Blade::directive('formClose', function ($expression) {
             return "<?php echo ($expression)->close(); ?>";
+        });
+
+        // Render a form field.
+        // First parameter is the form instance and the
+        // second parameter is a string representing the field name
+        // without a prefix - as registered when building the form.
+        Blade::directive('formField', function ($expression) {
+            list($form, $fieldName) = array_map('trim', explode(',', $expression));
+
+            return "<?php echo ($form)->repository()->getFieldByName($fieldName)->render(); ?>";
         });
     }
 }
