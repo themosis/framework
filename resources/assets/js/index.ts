62fcb636760427abd3c59cb9934fdd8497300238
Julien Lambé <julien@themosis.com>
2019-06-21 14:06:48 +0200

    Fix #693. Identify framework API requests.

--- resources/assets/js/index.ts
+++ resources/assets/js/index.ts
@@ -1,4 +1,5 @@
 import "@babel/polyfill/noConflict";
+import axios from "axios";
 import Manager from "./src/components/Manager";
 import MetaboxFactory from "./src/components/MetaboxFactory";
 import TextField from "./src/components/fields/TextField";
@@ -15,6 +16,12 @@ import MediaField from "./src/components/fields/MediaField";
 import CollectionField from "./src/components/fields/CollectionField";
 import EditorField from "./src/components/fields/EditorField";
 import '../sass/index.scss';
+
+/**
+ * Axios common settings
+ * Identify API requests performed by the framework.
+ */
+axios.defaults.headers.common['Themosis-Api-Request'] = 1;
 
 /*
  * Initialize the components Manager.
