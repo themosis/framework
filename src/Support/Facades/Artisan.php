f0acbc383c88824ad32ef146219b0d9234c909b9
Julien Lambé <julien@themosis.com>
2019-12-20 15:36:13 +0100

    Fix Artisan facade.

--- src/Support/Facades/Artisan.php
+++ src/Support/Facades/Artisan.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Themosis\Core\Support\Facades;
+namespace Themosis\Support\Facades;
 
 use Illuminate\Contracts\Console\Kernel as ConsoleKernelContract;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * Class Console
+ * Class Artisan
  *
  * @method static int handle(\Symfony\Component\Console\Input\InputInterface $input, \Symfony\Component\Console\Output\OutputInterface $output = null)
  * @method static int call(string $command, array $parameters = [], $outputBuffer = null)
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Facade;
  *
  * @see \Illuminate\Contracts\Console\Kernel
  */
-class Console extends Facade
+class Artisan extends Facade
 {
     /**
      * Get the registered name of the component.
