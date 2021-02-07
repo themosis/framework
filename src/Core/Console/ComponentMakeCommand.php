bb880ab83129a0203057016c9c5a1dd72d591f2a
Julien Lambé <julien@themosis.com>
2021-02-07 21:55:42 +0100

    Implement make component command.

---
+++ src/Core/Console/ComponentMakeCommand.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace Themosis\Core\Console;
+
+use Illuminate\Console\GeneratorCommand;
+use Illuminate\Support\Str;
+use Symfony\Component\Console\Input\InputOption;
+use Themosis\Core\HelloDolly;
+
+class ComponentMakeCommand extends GeneratorCommand
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'make:component';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new view component class';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Component';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        if (parent::handle() === false && ! $this->option('force')) {
+            return false;
+        }
+
+        if (! $this->option('inline')) {
+            $this->writeView();
+        }
+    }
+
+    /**
+     * Write the view for the component.
+     */
+    protected function writeView()
+    {
+        $path = $this->viewPath(
+            str_replace('.', '/', 'components.'.$this->getView()).'.blade.php'
+        );
+
+        if (! $this->files->isDirectory(dirname($path))) {
+            $this->files->makeDirectory(dirname($path), 0777, true, true);
+        }
+
+        if ($this->files->exists($path) && ! $this->option('force')) {
+            $this->error('View already exists!');
+
+            return;
+        }
+
+        file_put_contents(
+            $path,
+            '<div>
+    <!-- '.HelloDolly::lyric().' -->
+</div>'
+        );
+    }
+
+    /**
+     * Get the view name relative to the components directory.
+     *
+     * @return string view
+     */
+    protected function getView()
+    {
+        $name = str_replace('\\', '/', $this->argument('name'));
+
+        return collect(explode('/', $name))
+            ->map(function ($part) {
+                return Str::kebab($part);
+            })
+            ->implode('.');
+    }
+
+    /**
+     * Build the class with the given name.
+     *
+     * @param string $name
+     *
+     * @return string
+     */
+    protected function buildClass($name)
+    {
+        if ($this->option('inline')) {
+            return str_replace(
+                'DummyView',
+                "<<<'blade'\n<div>\n    <!-- ".HelloDolly::lyric()." -->\n</div>\nblade",
+                parent::buildClass($name)
+            );
+        }
+
+        return str_replace(
+            'DummyView',
+            'view(\'components.'.$this->getView().'\')',
+            parent::buildClass($name)
+        );
+    }
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        return __DIR__.'/stubs/view-component.stub';
+    }
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param string $rootNamespace
+     *
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace.'\View\Components';
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [
+            ['force', null, InputOption::VALUE_NONE, 'Create the class even if the component already exists'],
+            ['inline', null, InputOption::VALUE_NONE, 'Create a component that renders an inline view'],
+        ];
+    }
+}
