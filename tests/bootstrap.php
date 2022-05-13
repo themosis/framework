eb442067a996099baae278fd76f37eda1e0de700
Julien <julien@themosis.com>
2022-05-13 09:46:15 +0200

    Unit tests with WordPress WIP.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,6 +8,11 @@ define('CONTENT_DIR', 'content');
 define('WP_CONTENT_DIR', realpath(THEMOSIS_ROOT . DS . THEMOSIS_PUBLIC_DIR . DS . CONTENT_DIR));
 
 define('ABSPATH', __DIR__ . '/../vendor/johnpbloch/wordpress-core/');
+define('WP_SITEURL', 'https://themosis.test');
+define('WP_HOME', 'https://themosis.test');
+define('WP_CONTENT_URL', 'https://themosis.test/content');
+
+define('WP_DEFAULT_THEME', 'themosis-fake-theme');
 
 $table_prefix = 'wp_';
 
@@ -18,12 +23,39 @@ define('DB_HOST', 'localhost');
 define('DB_CHARSET', 'utf8mb4');
 define('DB_COLLATE', 'utf8mb4_unicode_ci');
 
+define('WP_INSTALLING', true);
+
+// Fake we're using SSL.
+$_SERVER['HTTPS'] = 1;
+
 if (file_exists($autoload = realpath(THEMOSIS_ROOT . '/../../vendor/autoload.php'))) {
     require $autoload;
 }
 
 // Verify WordPress is installed before proceeding further...
+if (function_exists('error_reporting')) {
+    /*
+     * Initialize error reporting to a known set of levels.
+     *
+     * This will be adapted in wp_debug_mode() located in wp-includes/load.php based on WP_DEBUG.
+     * @see http://php.net/manual/en/errorfunc.constants.php List of known error levels.
+     */
+    error_reporting(E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_ERROR | E_WARNING | E_PARSE | E_USER_ERROR | E_USER_WARNING | E_RECOVERABLE_ERROR);
+}
 
 if (file_exists($wpSettings = realpath(__DIR__ . '/../vendor/johnpbloch/wordpress-core/wp-settings.php'))) {
     require $wpSettings;
 }
+
+if (file_exists($wpUpgrade = realpath(__DIR__.'/../vendor/johnpbloch/wordpress-core/wp-admin/includes/upgrade.php'))) {
+    require $wpUpgrade;
+}
+
+wp_install(
+    'Themosis Tests',
+    'phpunit',
+    'support@themosis.com',
+    false,
+    '',
+    wp_generate_password(12, false),
+);
