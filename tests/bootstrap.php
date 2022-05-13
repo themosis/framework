bcf4255f80978931a4293b055490248eb22679d2
Julien <julien@themosis.com>
2022-05-13 18:38:17 +0200

    WIP.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,29 +4,6 @@ define('DS', DIRECTORY_SEPARATOR);
 
 define('THEMOSIS_PUBLIC_DIR', 'public');
 define('THEMOSIS_ROOT', realpath(__DIR__ . '/application'));
-define('CONTENT_DIR', 'content');
-define('WP_CONTENT_DIR', realpath(THEMOSIS_ROOT . DS . THEMOSIS_PUBLIC_DIR . DS . CONTENT_DIR));
-
-define('ABSPATH', __DIR__ . '/../vendor/johnpbloch/wordpress-core/');
-define('WP_SITEURL', 'https://themosis.test');
-define('WP_HOME', 'https://themosis.test');
-define('WP_CONTENT_URL', 'https://themosis.test/content');
-
-define('WP_DEFAULT_THEME', 'themosis-fake-theme');
-
-$table_prefix = 'wp_';
-
-define('DB_NAME', 'themosis_tests');
-define('DB_USER', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_HOST', 'localhost');
-define('DB_CHARSET', 'utf8mb4');
-define('DB_COLLATE', 'utf8mb4_unicode_ci');
-
-define('WP_INSTALLING', true);
-
-// Fake we're using SSL.
-$_SERVER['HTTPS'] = 1;
 
 if (file_exists($autoload = realpath(THEMOSIS_ROOT . '/../../vendor/autoload.php'))) {
     require $autoload;
